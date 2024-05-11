e5a3435754d57047556986a5d7651d5b5e460cc0
Vikky Ambarkar <vikkya94@yahoo.com>
2024-05-11 12:22:46 +0000

    added lambda to insert data to rds

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -1,17 +1,21 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
+    user_display_name = user['name']
+    user_email = user['email']
+    user_handle = user['preferred_username']
+    user_cognito_id = user['sub']
     try:
-        conn = psycopg2.connect(
-            host=(os.getenv('PG_HOSTNAME')),
-            database=(os.getenv('PG_DATABASE')),
-            user=(os.getenv('PG_USERNAME')),
-            password=(os.getenv('PG_SECRET'))
-        )
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         cur = conn.cursor()
-        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
+        sql = f"""
+            INSERT INTO users (display_name, email, handle, cognito_user_id) 
+            VALUES('{user_display_name}', '{user_email}', '{user_handle}', '{user_cognito_id}')
+        """
+        cur.execute(sql)
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
